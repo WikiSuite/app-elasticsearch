c62c6f7e16d777de4158ee7dbc7dc33babafe21d
pcbaldwin <peter@egloo.ca>
2018-05-01 14:49:36 -0400

    Buildsys retry

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'elasticsearch';
-$app['version'] = '1.2.5';
+$app['version'] = '1.2.6';
 $app['release'] = '1';
 $app['vendor'] = 'WikiSuite';
 $app['packager'] = 'eGloo';
